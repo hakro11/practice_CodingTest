82902f4930732feae0ad15de708f6a58a1b8fa7d
hakro11 <116020914+hakro11@users.noreply.github.com>
2023-01-29 01:00:05 +0900

    [level 0] Title: 숫자 찾기, Time: 6.58 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.java
@@ -1,13 +1,11 @@
 class Solution {
     public int solution(int num, int k) {
-        int answer = 0;
+        int answer = -1;
         String strnum = num + "";
 		String strk = k + "";
 		
 		if(strnum.indexOf(strk) != -1) {
 			answer = strnum.indexOf(strk) + 1;
-		}else {
-			answer = -1;
 		}
         
         return answer;
