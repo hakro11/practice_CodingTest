b5e5bf729c13ae140f4cdfc3cf62689bae5164a9
hakro11 <116020914+hakro11@users.noreply.github.com>
2023-04-04 10:22:44 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.02 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -1,16 +1,16 @@
 class Solution {
     public long solution(long n) {
         long answer = -1;
-        
+
         long i = 1;
-		while(true) {
-			if(n == i * i) {
-				answer = (i+1) * (i+1);
-				break;
-			}
-			i++;
-			if(i >= 10000000) break;
-		}
+        while(true) {
+            if(n == i * i) {
+                answer = (i+1) * (i+1);
+                break;
+            }
+            i++;
+            if(i >= 10000000) break;
+        }
         
         return answer;
     }
