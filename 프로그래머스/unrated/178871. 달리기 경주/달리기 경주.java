e70a304e73ed92fad6dd319603555fa0a6df6ea1
hakro11 <116020914+hakro11@users.noreply.github.com>
2023-05-04 20:01:46 +0900

    [unrated] Title: 달리기 경주, Time: 0.06 ms, Memory: 71.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] players, String[] callings) {
+       String[] answer = players.clone();
+        Map<String, Integer> rank =setRank(players);
+
+        for(int i = 0, idx; i < callings.length; i++){
+            idx = rank.get(callings[i]);
+            
+            rank.replace(callings[i], idx, idx-1);
+            rank.replace(answer[idx-1], rank.get(answer[idx-1]), rank.get(answer[idx-1])+1);
+            
+            answer[idx] = answer[idx-1];
+            answer[idx-1] = callings[i];
+
+        }
+
+        return answer;
+    }
+
+    public static Map<String, Integer> setRank(String[] players){
+        Map<String, Integer> ranking = new HashMap<>();
+
+        for(int i = 0; i < players.length; i++){
+            ranking.put(players[i], i);
+        }
+
+        return ranking;
+    }
+}
