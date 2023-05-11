9856b3f2874e9dce15063d35ab44039f3d49fdd5
hakro11 <116020914+hakro11@users.noreply.github.com>
2023-05-11 10:53:58 +0900

    [unrated] Title: 무인도 여행, Time: 1.12 ms, Memory: 73.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/154540. 무인도 여행/무인도 여행.java
@@ -0,0 +1,74 @@
+import java.util.*;
+
+class Solution {
+    static boolean[][] visited;
+    static int[][] dir = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+    public int[] solution(String[] maps) {
+        int cnt = countIslands(maps);
+        if (cnt == 0) return new int[]{-1};
+
+        int[] answer = new int[cnt];
+
+        int idx = 0;
+        visited = new boolean[maps.length][maps[0].length()];
+
+        for (int i = 0; i < maps.length; i++) {
+            for (int j = 0; j < maps[i].length(); j++) {
+                if (maps[i].charAt(j) != 'X' && !visited[i][j]) {
+                    answer[idx++] = bfs(i, j, maps);
+                }
+            }
+        }
+
+        Arrays.sort(answer);
+
+        return answer;
+    }
+
+    private static int bfs(int x, int y, String[] maps) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{x, y});
+
+        int sum = 0;
+
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            int curX = cur[0];
+            int curY = cur[1];
+
+            if (visited[curX][curY]) continue;
+            visited[curX][curY] = true;
+
+            sum += maps[curX].charAt(curY) - '0';
+
+            for (int i = 0; i < dir.length; i++) {
+                int nextX = curX + dir[i][0];
+                int nextY = curY + dir[i][1];
+
+                if (nextX < 0 || nextX >= maps.length || nextY < 0 || nextY >= maps[0].length()) continue;
+                if (visited[nextX][nextY] || maps[nextX].charAt(nextY) == 'X') continue;
+
+                queue.offer(new int[]{nextX, nextY});
+            }
+        }
+
+        return sum;
+    }
+
+    private static int countIslands(String[] maps) {
+        int cnt = 0;
+        visited = new boolean[maps.length][maps[0].length()];
+
+        for (int i = 0; i < maps.length; i++) {
+            for (int j = 0; j < maps[i].length(); j++) {
+                if (maps[i].charAt(j) != 'X' && !visited[i][j]) {
+                    bfs(i, j, maps);
+                    cnt++;
+                }
+            }
+        }
+
+        return cnt;
+    }
+}
