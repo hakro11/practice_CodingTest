8ae4402d3a81bc48519a8b40741bbe6052495dd1
hakro11 <116020914+hakro11@users.noreply.github.com>
2023-05-26 11:24:55 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42842. 카펫/카펫.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int total = brown + yellow;
+        
+        return locationList(total, brown);
+    }
+    
+    public int[] locationList(int total, int brown){
+        int[] answer = new int[2]; 
+        int stNum = 3;
+        int x = 0;
+        int y = 0;
+        
+        int i = 0;
+        while(true){
+            if(stNum >= total) break;
+            
+            if(total % stNum == 0 ){
+                x = total / stNum;
+                y = stNum;
+                
+                if(2*x + (y-2)*2 == brown) break;
+                if(y > x) break;
+            }
+            
+            stNum++;
+        }
+        
+        answer[0] = x;
+        answer[1] = y;
+        
+        return answer;
+    }
+
+}
