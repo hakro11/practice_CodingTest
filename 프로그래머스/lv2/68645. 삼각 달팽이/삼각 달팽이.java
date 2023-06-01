7921c26898393dbc1e2c54d2279b472b39097d88
hakro11 <116020914+hakro11@users.noreply.github.com>
2023-06-01 11:57:54 +0900

    [level 2] Title: 삼각 달팽이, Time: 41.43 ms, Memory: 149 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+class Solution {
+    int value = 0;
+    int height;
+    int totalCycle = 0;
+
+    public int[] solution(int n) {
+        List<Integer> list = new ArrayList<>();
+        int[][] dataMap = new int[n][];
+        this.height = n;
+
+        for(int i = 0; i < dataMap.length;i++){
+            dataMap[i] = new int[i+1];
+        }
+
+        cycle1(dataMap);
+        readValue(dataMap, list);
+        
+        return list.stream().mapToInt(i -> i).toArray();
+    }
+
+    public void readValue(int[][] dataMap, List list) {
+        for (int i = 0; i < dataMap.length; i++) {
+            for (int j = 0; j < dataMap[i].length; j++) {
+                list.add(dataMap[i][j]);
+            }
+        }
+    }
+
+    public void cycle1(int[][] dataMap) {
+        int cnt = 0;
+        for (int i = totalCycle; i < height - totalCycle; i++) {
+            if (dataMap[i][totalCycle] == 0) {
+                dataMap[i][totalCycle] = ++value;
+                cnt++;
+            }
+        }
+        if (cnt == 0) return;
+
+        for (int i = totalCycle+1, n = height - totalCycle-1; i < n; i++) {
+            if (dataMap[n][i] == 0) {
+                dataMap[n][i] = ++value;
+                cnt++;
+            }
+        }
+        if(cnt==0) return;
+
+        int x = height-totalCycle-1, y = height-(2*totalCycle)-1;
+        while (true){
+            if( x < totalCycle*2 && y < totalCycle) break;
+
+            if(dataMap[x][y] == 0 ){
+                dataMap[x][y] = ++value;
+                cnt++;
+            }
+            x--;
+            y--;
+        }
+
+        if(cnt == 0) return;
+
+        totalCycle++;
+        cycle1(dataMap);
+    }
+}
