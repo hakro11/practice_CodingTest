b08a61097e4658db1ccb3db65578702bc2288a14
hakro11 <116020914+hakro11@users.noreply.github.com>
2023-06-05 12:20:17 +0900

    [unrated] Title: 달리기 경주, Time: 0.04 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.java
+++ 프로그래머스/unrated/178871. 달리기 경주/달리기 경주.java
@@ -2,32 +2,30 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String[] players, String[] callings) {
-       String[] answer = players.clone();
-        Map<String, Integer> rank =setRank(players);
-
+        String[] answer = players.clone();
+        Map<String, Integer> map = makeMap(players);
+        
         for(int i = 0, idx; i < callings.length; i++){
-            idx = rank.get(callings[i]);
-            
-            rank.replace(callings[i], idx, idx-1);
-            rank.replace(answer[idx-1], rank.get(answer[idx-1]), rank.get(answer[idx-1])+1);
-            
+            idx = map.get(callings[i]);
+
+            map.replace(callings[i], idx-1);
+            map.replace(answer[idx-1], map.get(answer[idx-1])+1);
+
             answer[idx] = answer[idx-1];
             answer[idx-1] = callings[i];
 
         }
         
-        rank.clear();
-
         return answer;
     }
-
-    public static Map<String, Integer> setRank(String[] players){
-        Map<String, Integer> ranking = new HashMap<>();
-
+    
+    public Map<String, Integer> makeMap(String[] players){
+        Map<String, Integer> make = new HashMap<>();
+        
         for(int i = 0; i < players.length; i++){
-            ranking.put(players[i], i);
+            make.put(players[i], i);
         }
-
-        return ranking;
+        
+        return make;
     }
 }
